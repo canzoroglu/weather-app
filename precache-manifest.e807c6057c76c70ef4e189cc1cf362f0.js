e7ff072e42b96076bc16f2d69b82f4e704e8ccd2
canzoroglu <canzoroglu@gmail.com>
2019-05-12 18:04:36 +0300

    Updates

---
+++ precache-manifest.e807c6057c76c70ef4e189cc1cf362f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f779ee5c8818a0ccd56a57e94fb54fcd",
+    "url": "/canzoroglu/weather-app/index.html"
+  },
+  {
+    "revision": "39c05d7af547b1708dcc",
+    "url": "/canzoroglu/weather-app/static/css/main.76ddbed0.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/canzoroglu/weather-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "39c05d7af547b1708dcc",
+    "url": "/canzoroglu/weather-app/static/js/main.d8a5d81c.chunk.js"
+  },
+  {
+    "revision": "326bb3c689cd814c7547",
+    "url": "/canzoroglu/weather-app/static/js/runtime~main.0effc5a0.js"
+  }
+]);
